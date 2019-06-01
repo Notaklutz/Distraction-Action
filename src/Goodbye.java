fb289c4b20ada4eb89863f2cf7658ea665de56a8
Notaklutz <49736150+Notaklutz@users.noreply.github.com>
2019-06-01 11:58:42 -0400

    Add files via upload

--- src/Goodbye.java
+++ src/Goodbye.java
@@ -2,60 +2,76 @@ import java.awt.*;
 import javax.swing.*;
 import java.io.*;
 /*
-@author Kevin Nguyen
-@version 1 - May 14, 2019
-<p>
-This class will thank the user for playing the game.
-</p>
-*/
+ * @author Kevin Nguyen
+ * @version 1 - May 14, 2019
+ * 
+ * <h2>Course Info:</h2>
+ * ICS4U0 with Krasteva, V.
+ * 
+ * <p>
+ * This class will thank the user for playing the game. It extends TextOnly.
+ * </p>
+ */
 public class Goodbye extends TextOnly
 {
- /**
-  * This constructor will create a Goodbye object. It will initialize the custom fonts 
-  * and register them to the Graphics environment as well as initialize the layout of the panel.
-  * It will also call the printTitle() method, the printText() method, and the printFooter() method.
-  */   
-  public Goodbye()
+  /**
+   * This constructor will create a Goodbye object and assign the passed in parameter to game. It will initialize the custom fonts 
+   * and register them to the Graphics environment as well as initialize the layout of the panel.
+   * It will also call the printTitle() method, the printText() method, and the printFooter() method.
+   * 
+   * @param d The DistractionAction object that will contain the frame that goodbye will be displayed on.
+   */   
+  public Goodbye(DistractionAction d)
   {
+    game = d;
     initializeFontsAndLayout();
-    ge.registerFont (bigTitle);
-    ge.registerFont (smallTitle);
-    ge.registerFont (defaultFont);
     printTitle();
     printText();
     printFooter();
-    this.setBackground(DistractionAction.grey);
+    this.setBackground(DistractionAction.GREY);
     this.setVisible(true);
   }
   
- /**
-  * This method will print a goodbye message to the user.
-  */   
+  /**
+   * This method will print a goodbye message to the user.
+   * <p>
+   * <pre>
+   * Variable Name            Variable Type        Description
+   * goodbye                  JLabel               The JLabel that will display "GOODBYE!".
+   * thank                    JLabel               The JLabel that will display "THANK YOU FOR PLAYING".
+   * learned                  JLabel               The JLabel that will display "WE HOPE YOU LEARNED SOMETHING".
+   * play                     JLabel               The JLabel that will display "PLAY AGAIN ANYTIME!".
+   * </pre>
+   * </p>
+   */   
   public void printText()
   {
-   JLabel goodbye = new JLabel ("GOODBYE!", JLabel.CENTER); 
-   goodbye.setFont (defaultFont);
-   goodbye.setForeground(Color.white);
-   layout.putConstraint(SpringLayout.WEST, goodbye, 400, SpringLayout.WEST, DistractionAction.frame);
-   layout.putConstraint(SpringLayout.NORTH, goodbye, 70, SpringLayout.NORTH, DistractionAction.frame);
-   this.add(goodbye);
-   JLabel thank = new JLabel ("THANK YOU FOR PLAYING", JLabel.CENTER);
-   thank.setFont (defaultFont);
-   thank.setForeground(Color.white);
-   layout.putConstraint(SpringLayout.WEST, thank, 250, SpringLayout.WEST, DistractionAction.frame);
-   layout.putConstraint(SpringLayout.NORTH, thank, 100, SpringLayout.SOUTH, goodbye);
-   this.add(thank);
-   JLabel learned = new JLabel ("WE HOPE YOU LEARNED SOMETHING", JLabel.CENTER);
-   learned.setFont (defaultFont);
-   learned.setForeground(Color.white);
-   layout.putConstraint(SpringLayout.WEST, learned, 200, SpringLayout.WEST, DistractionAction.frame);
-   layout.putConstraint(SpringLayout.NORTH, learned, 50, SpringLayout.SOUTH, thank);
-   this.add(learned);
-   JLabel play = new JLabel ("PLAY AGAIN ANYTIME!", JLabel.CENTER);
-   play.setFont (defaultFont);
-   play.setForeground(Color.white);
-   layout.putConstraint(SpringLayout.WEST, play, 280, SpringLayout.WEST, DistractionAction.frame);
-   layout.putConstraint(SpringLayout.NORTH, play, 50, SpringLayout.SOUTH, learned);
-   this.add(play);
+    JLabel goodbye = new JLabel ("GOODBYE!", JLabel.CENTER); 
+    goodbye.setFont (highScoresAndGoodbyeFont);
+    goodbye.setForeground(Color.white);
+    layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, goodbye, 0, SpringLayout.HORIZONTAL_CENTER, this);
+    layout.putConstraint(SpringLayout.NORTH, goodbye, 90, SpringLayout.NORTH, game.frame);
+    this.add(goodbye);
+    
+    JLabel thank = new JLabel ("THANK YOU FOR PLAYING", JLabel.CENTER);
+    thank.setFont (highScoresAndGoodbyeFont);
+    thank.setForeground(Color.white);
+    layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, thank, 0, SpringLayout.HORIZONTAL_CENTER, this);
+    layout.putConstraint(SpringLayout.NORTH, thank, 100, SpringLayout.SOUTH, goodbye);
+    this.add(thank);
+    
+    JLabel learned = new JLabel ("WE HOPE YOU LEARNED SOMETHING", JLabel.CENTER);
+    learned.setFont (highScoresAndGoodbyeFont);
+    learned.setForeground(Color.white);
+    layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, learned, 0, SpringLayout.HORIZONTAL_CENTER, this);
+    layout.putConstraint(SpringLayout.NORTH, learned, 50, SpringLayout.SOUTH, thank);
+    this.add(learned);
+    
+    JLabel play = new JLabel ("PLAY AGAIN ANYTIME!", JLabel.CENTER);
+    play.setFont (highScoresAndGoodbyeFont);
+    play.setForeground(Color.white);
+    layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, play, 0, SpringLayout.HORIZONTAL_CENTER, this);
+    layout.putConstraint(SpringLayout.NORTH, play, 50, SpringLayout.SOUTH, learned);
+    this.add(play);
   }
 }
--- src/TextOnly.java
+++ src/TextOnly.java
@@ -1,142 +1,165 @@
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
-
 /*
-@author Kevin Nguyen
-@version 1 - May 14, 2019
-<p>
-This abstract class will be the parent class of any classes that are mostly text-based,
-with a method to print the title and an abstract method to print text.
-</p>
-*/
-public abstract class TextOnly extends JPanel {
-    /**
-     * The graphics environment of the class.
-     */
-    protected GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-    /**
-     * The variable that stores an object of the SpringLayout class.
-     */
-    protected SpringLayout layout = new SpringLayout();
-    /**
-     * The custom font for the large letters in the game logo.
-     */
-    protected Font bigTitle;
-    /**
-     * The custom font for the small letters in the game logo.
-     */
-    protected Font smallTitle;
-    /**
-     * The custom font for the buttons in the main menu and level selection screens.
-     */
-    protected Font buttonFont;
-    /**
-     * The font for the high scores screen.
-     */   
-    protected Font highScoresFont;
-    /**
-     * The default font of the game.
-     */
-    protected Font defaultFont;
-    /**
-     * The dark grey colour that is used in the game's background.
-     */
-    protected static final Color GREY = new Color(64, 64, 64);
-    /**
-     * The light grey colour that is used in the game's text.
-     */
-    protected final Color LIGHT_GREY = new Color(190, 190, 190);
-    /**
-     * The yellow colour that is used in the game's text.
-     */
-    protected final Color YELLOW = new Color(255, 205, 0);
-    /**
-     * The red colour that is used in the game's text.
-     */
-    protected final Color RED = new Color(194, 85, 85);
-    /**
-     * The JLabel for the big D in the game's logo.
-     */
-    protected JLabel bigD = new JLabel();
-    /**
-     * The JLabel for the regular words in the game's logo.
-     */
-    protected JLabel midTitle = new JLabel();
-    /**
-     * The JLabel for the big N in the game's logo.
-     */
-    protected JLabel bigN = new JLabel();
-    /**
-     * The JLabel for the first part of the footer ("2019 KODIAK").
-     */
-    protected JLabel footer1 = new JLabel();
-    /**
-     * The JLabel for the second part of the footer ("RED").
-     */
-    protected JLabel footer2 = new JLabel();
-
-    /**
-     * This method will initialize the custom fonts that are needed for the game as well as the panel's layout.
-     */
-    public void initializeFontsAndLayout() {
-        try {
-            bigTitle = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts/Apple.ttf")).deriveFont(Font.BOLD, 80f);
-            smallTitle = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts/Apple.ttf")).deriveFont(Font.BOLD, 47f);
-            buttonFont = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts/Apple.ttf")).deriveFont(Font.BOLD, 40f);
-            highScoresFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(30f);
-            defaultFont = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts/Apple.ttf")).deriveFont(20f);
-        } catch (IOException | FontFormatException e) 
-        {
-        }
-        this.setLayout(layout);
+ * @author Kevin Nguyen
+ * @version 1 - May 14, 2019
+ * 
+ * <h2>Course Info:</h2>
+ * ICS4U0 with Krasteva, V.
+ * 
+ * <p>
+ * This abstract class will be the parent class of any classes that are mostly text-based,
+ * with a method to print the title and an abstract method to print text. It extends JPanel.
+ * </p>
+ */
+public abstract class TextOnly extends JPanel
+{
+  /**
+   * The variable that stores an object of the SpringLayout class.
+   */   
+  protected SpringLayout layout = new SpringLayout();
+  /**
+   * The custom font for the large letters in the game logo.
+   */   
+  protected Font bigTitle;
+  /**
+   * The custom font for the small letters in the game logo.
+   */   
+  protected Font smallTitle;
+  /**
+   * The custom font for the buttons in the main menu, level selection, and high score screens.
+   */
+  protected Font buttonFont;
+  /**
+   * The font for the high scores and goodbye screens.
+   */   
+  protected Font highScoresAndGoodbyeFont;
+  /**
+   * The font for the choices in the quiz.
+   */   
+  protected Font choiceFont;
+  /**
+   * The font for the game's book in level one.
+   */   
+  protected Font bookFont;
+  /**
+   * The default font of the game.
+   */   
+  protected Font defaultFont;
+  /**
+   * The font for level 1's quiz.
+   */   
+  protected Font quizFont;
+  /**
+   * The light grey colour that is used in the game's text.
+   */   
+  protected final Color LIGHT_GREY = new Color (190, 190, 190); 
+  /**
+   * The yellow colour that is used in the game's text.
+   */   
+  protected final Color YELLOW = new Color (255, 205, 0);
+  /**
+   * The red colour that is used in the game's text.
+   */   
+  protected final Color RED = new Color (194, 85, 85);   
+  /**
+   * The JLabel for the big D in the game's logo.
+   */   
+  protected JLabel bigD = new JLabel();
+  /**
+   * The JLabel for the regular words in the game's logo.
+   */   
+  protected JLabel midTitle = new JLabel();
+  /**
+   * The JLabel for the big N in the game's logo.
+   */   
+  protected JLabel bigN = new JLabel();
+  /**
+   * The JLabel for the first part of the footer ("2019 KODIAK").
+   */   
+  protected JLabel footer1 = new JLabel();
+  /**
+   * The JLabel for the second part of the footer ("RED").
+   */   
+  protected JLabel footer2 = new JLabel();
+  /**
+   * The DistractionAction object that will contain the frame that all of the screens will be displayed on.
+   */   
+  protected DistractionAction game;
+  
+  /**
+   * This method will initialize the custom fonts that are needed for the game as well as the panel's layout.
+   */
+  public void initializeFontsAndLayout()
+  {
+    try
+    {
+      bigTitle = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(Font.BOLD, 80f);
+      smallTitle = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(Font.BOLD, 47f);
+      buttonFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(Font.BOLD, 40f);
+      highScoresAndGoodbyeFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(30f);
+      bookFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(13f);
+      defaultFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(20f);
+      quizFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(25f);
+      choiceFont = Font.createFont(Font.TRUETYPE_FONT, new File("Apple.ttf")).deriveFont(27f);
     }
-
-    /**
-     * This method will print the logo of the game, Distraction Action.
-     */
-    public void printTitle() {
-        bigD.setFont(bigTitle);
-        bigD.setForeground(YELLOW);
-        bigD.setText("D");
-        layout.putConstraint(SpringLayout.WEST, bigD, 20, SpringLayout.WEST, DistractionActionDriver.frame);
-        layout.putConstraint(SpringLayout.NORTH, bigD, 10, SpringLayout.NORTH, DistractionActionDriver.frame);
-        this.add(bigD);
-        midTitle.setFont(smallTitle);
-        midTitle.setForeground(YELLOW);
-        midTitle.setText("ISTRACTION ACTIO");
-        layout.putConstraint(SpringLayout.WEST, midTitle, -10, SpringLayout.EAST, bigD);
-        layout.putConstraint(SpringLayout.NORTH, midTitle, 10, SpringLayout.NORTH, DistractionActionDriver.frame);
-        this.add(midTitle);
-        bigN.setFont(bigTitle);
-        bigN.setForeground(YELLOW);
-        bigN.setText("N");
-        layout.putConstraint(SpringLayout.WEST, bigN, -5, SpringLayout.EAST, midTitle);
-        layout.putConstraint(SpringLayout.NORTH, bigN, 10, SpringLayout.NORTH, DistractionActionDriver.frame);
-        this.add(bigN);
-    }
-
-    /**
-     * This method will print "2019 KODIAK RED" at the bottom of the screen.
-     */
-    public void printFooter() {
-        footer1.setFont(defaultFont);
-        footer1.setForeground(LIGHT_GREY);
-        footer1.setText("2019 KODIAK");
-        layout.putConstraint(SpringLayout.WEST, footer1, 340, SpringLayout.WEST, DistractionActionDriver.frame);
-        layout.putConstraint(SpringLayout.SOUTH, footer1, 750, SpringLayout.NORTH, DistractionActionDriver.frame);
-        this.add(footer1);
-        footer2.setFont(defaultFont);
-        footer2.setForeground(RED);
-        footer2.setText("RED");
-        layout.putConstraint(SpringLayout.WEST, footer2, 15, SpringLayout.EAST, footer1);
-        layout.putConstraint(SpringLayout.SOUTH, footer2, 750, SpringLayout.NORTH, DistractionActionDriver.frame);
-        this.add(footer2);
-    }
-
-    /**
-     * This is a generic print method that will be overridden by TextOnly's subclasses.
-     */
-    public abstract void printText();
+    catch (IOException|FontFormatException e) 
+    {
+    } 
+    this.setLayout(layout);
+  }
+  
+  /**
+   * This method will print the logo of the game, Distraction Action.
+   */
+  public void printTitle()
+  {
+    bigD.setFont(bigTitle);
+    bigD.setForeground(YELLOW);
+    bigD.setText("D");
+    layout.putConstraint(SpringLayout.WEST, bigD, 20, SpringLayout.WEST, game.frame);
+    layout.putConstraint(SpringLayout.NORTH, bigD, 10, SpringLayout.NORTH, game.frame);
+    this.add(bigD);
+    
+    midTitle.setFont(smallTitle);
+    midTitle.setForeground(YELLOW);
+    midTitle.setText("ISTRACTION ACTIO");
+    layout.putConstraint(SpringLayout.WEST, midTitle, -10, SpringLayout.EAST, bigD);
+    layout.putConstraint(SpringLayout.NORTH, midTitle, 10, SpringLayout.NORTH, game.frame);
+    this.add(midTitle);
+    
+    bigN.setFont(bigTitle);
+    bigN.setForeground(YELLOW);
+    bigN.setText("N");
+    layout.putConstraint(SpringLayout.WEST, bigN, -5, SpringLayout.EAST, midTitle);
+    layout.putConstraint(SpringLayout.NORTH, bigN, 10, SpringLayout.NORTH, game.frame);
+    this.add(bigN);
+  }
+  
+  /**
+   * This method will print "2019 KODIAK RED" at the bottom of the screen.
+   */
+  public void printFooter()
+  {
+    footer1.setFont(defaultFont);
+    footer1.setForeground(LIGHT_GREY);
+    footer1.setText("2019 KODIAK");
+    layout.putConstraint(SpringLayout.WEST, footer1, 340, SpringLayout.WEST, game.frame);
+    layout.putConstraint(SpringLayout.SOUTH, footer1, 750, SpringLayout.NORTH, game.frame);
+    this.add(footer1);
+    
+    footer2.setFont(defaultFont);
+    footer2.setForeground(RED);
+    footer2.setText("RED");
+    layout.putConstraint(SpringLayout.WEST, footer2, 15, SpringLayout.EAST, footer1);
+    layout.putConstraint(SpringLayout.SOUTH, footer2, 750, SpringLayout.NORTH, game.frame);
+    this.add(footer2);
+  }
+  
+  /**
+   * This is a generic print method that will be overriden by TextOnly's subclasses.
+   */
+  public abstract void printText();
 }
 
